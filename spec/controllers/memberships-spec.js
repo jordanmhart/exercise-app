c0ada27a39038012803be67ec13ac7efedb109e2
ddsooxo <dchuteng@gmail.com>
2015-10-23 11:50:02 -0700

    wrote test for memberships-spec.js file

---
+++ spec/controllers/memberships-spec.js
@@ -0,0 +1,95 @@
+var request = require('request');
+var Membership = require('../../app/models/membership');
+var MembershipsController = require('../../app/controllers/memberships');
+
+
+describe('MembershipsController', function() {
+
+  describe('tests without data', function() {
+    
+    //create new membership for the user
+    it('should create a new membership', function (done) {
+      var options = {
+        url: 'http://localhost:3000/group/1/invite',
+        form: {
+          user_id: 1,
+          membership: 'potato',
+          invite_accepted: false
+        }
+      };
+
+      request.post(options, function (error, response, body) {
+        expect(response.statusCode).toBe(302);
+        new Membership({
+          membership: 'potato'
+        })
+        .fetch()
+        .then(function (membership) {
+          expect(membership.get('id')).toBeDefined();
+          new Membership({
+            id: membership.id
+          })
+          .destroy()
+          .then(function(){
+            done();
+          })
+          .catch(function(error) {
+            console.log(error);
+            done.fail(error);
+          });
+        })
+        .catch(function(error){
+          console.log("second error catch");
+          done.fail(error);
+        })
+      });
+    });
+  });
+  
+
+ //  describe('tests with data', function() {
+ //    var membership;
+ //    beforeEach(function(done) {
+ //      new Membership({
+ //        user_id: 1,
+ //        group_id: 2,
+ //        membership: 'salad',
+ //        invite_accepted: false
+ //      })
+ //      .save()
+ //      .then(function(newMembership) {
+ //        membership = newMembership;
+ //        done();
+ //      });
+ //    });
+
+ //    afterEach(function(done) {
+ //      new Membership({
+ //        id: membership.id
+ //      }).destroy()
+ //        .then(done)
+ //        .catch(function(error) {
+ //          done.fail(error);
+ //        });
+ //    });
+    
+ //    //delete a membership
+ //    it('should delete a membership', function(done) {
+ //      var options = {
+ //        url: 'http://localhost:3000/membership/' + membership.id + '/delete'
+ //      };
+
+ //      request.post(options, function(error, response, body) {
+ //        expect(response.statusCode).toBe(302);
+ //        new Membership({
+ //          id: membership.id
+ //        })
+ //        .fetch()
+ //        .then(function(deletedMembership) {
+ //          expect(deletedMembership).toBeNull();
+ //          done();
+ //        });
+ //      });
+ //    });
+ // });
+});
