474ee45bfb18ffdee68dd5660bc9543644f7b055
Jordan Hart <jordan@jordanhart.com>
2015-11-02 14:16:03 -0800

    finished changing views and controllers, all working now

--- app/controllers/groups.js
+++ app/controllers/groups.js
@@ -83,7 +83,7 @@ Users.fetch()
           if(membership){
             res.render('groups/show', {
               user_id: req.user.get('id'),
-              membership: membership.toJSON().role,
+              role: membership.toJSON().role,
               group: group.toJSON()
             })
           }else{
@@ -126,7 +126,8 @@ exports.submitGroup = function (req, res){
     name: req.body.name,
     description: req.body.description,
     start_date: req.body.start_date,
-    end_date: req.body.end_date
+    end_date: req.body.end_date,
+    price: req.body.price
     // exercise_length: req.body.exercise_length,
     // days_per_week: req.body.days_per_week
   })
@@ -176,7 +177,8 @@ exports.submitEdit = function (req, res){
             name: req.body.name,
             description: req.body.description,
             start_date: req.body.start_date,
-            end_date: req.body.end_date
+            end_date: req.body.end_date,
+            price: req.body.price
         })
         .then(function (data){
             req.method = 'get';
